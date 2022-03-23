f48eb02b734c5c864be19d3145ad26b7526b43a3
alexnaev <102225427+alexnaev@users.noreply.github.com>
2022-03-23 14:11:18 +0100

    Initial commit

---
+++ app/views/shared/footer.php
@@ -0,0 +1,33 @@
+<footer class="footer mt-auto py-3 sticky-footer">
+    <div class="container-fluid">
+        <div class="row row-cols-1 row-cols-lg-3 gy-2">
+            <div class="col order-last order-lg-first text-light">
+                <div><span class="text-muted">&copy;2022</span> Alexander Nævdal</div>
+            </div>
+            <div class="col d-flex align-items-center justify-content-start justify-content-lg-center">LOGO</div>
+            <div class="col d-flex align-items-center justify-content-start justify-content-lg-end">
+                <div class="row">
+                    <div class="col social"><a href="#" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a></div>
+                    <div class="col social"><a href="#" target="_blank"><i class="fab fa-github fa-2x"></i></a></div>
+                    <div class="col social"><a href="#" target="_blank"><i class="fab fa-twitter fa-2x"></i></a></div>
+                    <div class="col social"><a href="#" target="_blank"><i class="fab fa-youtube fa-2x"></i></a></div>
+                </div>
+            </div>
+        </div>
+</footer>
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
+</script>
+<script src="js/site.js"></script>
+<script src="js/prism.js"></script>
+<script>
+    Prism.plugins.NormalizeWhitespace.setDefaults({
+        'remove-trailing': true,
+        'remove-indent': true,
+        'left-trim': true,
+        'right-trim': true
+    })
+</script>
+</body>
+
+</html>
